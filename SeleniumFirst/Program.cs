aabb5d8af7de3b9b3455f1149f700806b62008c2
rbanda <rbanda@IM-PC-167>
2016-08-17 19:21:24 +0530

    Add project files.

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        // Create Reference for Broser
+        
+
+        static void Main(string[] args)
+        {
+
+            
+        }
+
+        [SetUp]
+        public void Inititalize()
+        {
+            PropertiesCollection.driver = new ChromeDriver();
+
+            //Navigate to Google Page
+            PropertiesCollection.driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=username&Password=password&Login=Login");
+
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            SeleniumSetMethods.SelectDropDown("TitleId", "Mr.", PropertyType.Name);
+            SeleniumSetMethods.EnterText("Initial", "B", PropertyType.Name);
+            SeleniumSetMethods.EnterText("FirstName", "Rammohan", PropertyType.Name);
+
+            Console.WriteLine( "Dropdown Values are: " + SeleniumGetMethods.GetTextFromDDL("TitleId", PropertyType.Name) );
+            Console.WriteLine( "Initial is : " + SeleniumGetMethods.GetText("Initial", PropertyType.Name));
+            Console.WriteLine("First Name is : " + SeleniumGetMethods.GetText("FirstName", PropertyType.Name));
+   
+            SeleniumSetMethods.Click("Save", PropertyType.Name);
+        }
+
+      
+        [TearDown]
+        public void Close()
+        {
+            PropertiesCollection.driver.Close();
+            Console.WriteLine("Closed the Browser");
+        }
+    }
+}
---
+++ SeleniumFirst/SeleniumGetMethods.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class SeleniumGetMethods
+    {
+
+        public static string GetText(String Element, PropertyType Type)
+        {
+            if (Type == PropertyType.Id)
+                return PropertiesCollection.driver.FindElement(By.Id(Element)).GetAttribute("value");
+            if (Type == PropertyType.Name)
+                return PropertiesCollection.driver.FindElement(By.Name(Element)).GetAttribute("value");
+            else return string.Empty;
+        }
+
+        public static string GetTextFromDDL(String Element, PropertyType Type)
+        {
+            if (Type == PropertyType.Id)
+                return new SelectElement(PropertiesCollection.driver.FindElement(By.Id(Element))).AllSelectedOptions.Single().Text;
+            if (Type == PropertyType.Name)
+                return new SelectElement(PropertiesCollection.driver.FindElement(By.Name(Element))).AllSelectedOptions.Single().Text;
+            else return string.Empty;
+        }
+    }
+}
